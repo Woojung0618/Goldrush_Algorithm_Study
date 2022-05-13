488fba33a032825c13443afeba81812f118ee67a
Woojung0618 <woojung0618@gmail.com>
2022-05-14 03:06:16 +0900

    14425 실버3 문자열 집합

--- 8주차/집합/woojung.py
+++ 8주차/집합/woojung.py
@@ -1,12 +1,12 @@
 import sys
-S = []
+S = set([])
 print(S)
 m = int(input())
 for _ in range(m):
     cmd = sys.stdin.readline().rstrip().split()
     if cmd[0] == 'add':
         if cmd[1] not in S:
-            S.append(cmd[1])
+            S.add(cmd[1])
     elif cmd[0] == 'remove':
         if cmd[1] in S:
             S.remove(cmd[1])
@@ -19,9 +19,9 @@ for _ in range(m):
         if cmd[1] in S:
             S.remove(cmd[1])
         else:
-            S.append(cmd[1])
+            S.add(cmd[1])
     elif cmd[0] == 'all':
-        S = [i + 1 for i in range(20)]
+        S = set([i + 1 for i in range(20)])
     else:  # empty
         S = []
 
